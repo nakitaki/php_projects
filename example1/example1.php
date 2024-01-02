9fdf027be000064ed9b991f3c510b5e54480fa43
nakitaki <nadya.mishevaa@gmail.com>
2024-01-02 18:20:31 +0200

    new files

---
+++ example1/example1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        form{
+            width: 300px;
+            margin: 30% auto 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <form method="get" action="getsearch.php">
+        <input type="text">
+        <input type="submit">
+    </form>
+</body>
+</html>
