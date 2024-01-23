bcc61cce6ec8a48ab1daf99d733af30818079bb6
nakitaki <nadya.mishevaa@gmail.com>
2024-01-23 15:24:04 +0200

    final

---
+++ php_project - Copy/config/constants.php
@@ -0,0 +1,10 @@
+<?php
+//start session
+session_start();
+
+define('DB_SERVER', "localhost:3307");
+define('DB_USER', "root");
+define('DB_PASS', "");
+define('DB_NAME', "task_manager1");
+
+define('SITEURL', 'http://localhost/php_project/');
