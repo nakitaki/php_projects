cb7848f153422c27349834f11d84b2e85184f2c5
nakitaki <nadya.mishevaa@gmail.com>
2024-01-26 18:07:30 +0200

    config status

--- php_project/config/constants.php
+++ php_project/config/constants.php
@@ -5,6 +5,6 @@ session_start();
 define('DB_SERVER', "localhost:3307");
 define('DB_USER', "root");
 define('DB_PASS', "");
-define('DB_NAME', "task_manager1");
+define('DB_NAME', "task_manager_2");
 
 define('SITEURL', 'http://localhost/php_project/');
